9b91c5e6e86283914e4832cb35287953dc57664a
Rakesh D R <rakeshdr543@gmail.com>
2021-07-14 10:46:23 +0530

    Added array rotation improve

--- rotate-array.java
+++ rotate-array.java
@@ -1,7 +1,7 @@
 public static void rotate(int arr[], int n, int d){
-     int temp[]= new int[d];
+    int temp[]= new int[d];
 
-     for(int i=0;i<n;i++){
+     for(int i=0;i<d;i++){
          temp[i]=arr[i];
      }
 
