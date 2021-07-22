fb8405b3e273394c5ff6b2b3fa1bbb840ad68d57
Rakesh D R <rakeshdr543@gmail.com>
2021-07-22 11:31:43 +0530

    Added strange number

---
+++ strange9.js
@@ -0,0 +1,32 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readline() {
+    return inputString[currentLine++];
+}
+// Make a Snippet for the code above this and then write your logic in main();
+
+
+function main() {
+    const x = parseInt(readline());
+
+  // start a loop for all elements
+
+  // return the result
+  console.log(9*(x-1));
+}
