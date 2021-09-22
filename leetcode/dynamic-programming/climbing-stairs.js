cc2898251b154b309e19a3249f5c307c975a7eb1
Rakesh D R <rakeshdr543@gmail.com>
2021-09-22 11:05:17 +0530

    Added optimal climbing stairs

--- leetcode/dynamic-programming/climbing-stairs.js
+++ leetcode/dynamic-programming/climbing-stairs.js
@@ -3,11 +3,32 @@
  * @return {number}
  */
 
- function fib(n){
-    if(n<=1) return n
-    return fib(n-1)+fib(n-2)
-}
-
-var climbStairs = function(n) {
-    return fib(n+1)
+ var climbStairs = function(n) {
+    if(n==0 || n==1) return 1
+    
+    let first=1
+    let second=2
+    
+    for(let i=3;i<=n;i++){
+        let third=first + second
+        first=second
+        second=third
+    }
+    
+    return second
 };
+
+// -------------------------------------
+// /**
+//  * @param {number} n
+//  * @return {number}
+//  */
+
+//  function fib(n){
+//     if(n<=1) return n
+//     return fib(n-1)+fib(n-2)
+// }
+
+// var climbStairs = function(n) {
+//     return fib(n+1)
+// };
