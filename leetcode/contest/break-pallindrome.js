093b7c45052f7684abca1e62d54bad0de3838bff
Rakesh D R <rakeshdr543@gmail.com>
2021-09-24 11:22:09 +0530

    Added break pallindrome

---
+++ leetcode/contest/break-pallindrome.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} palindrome
+ * @return {string}
+ */
+ var breakPalindrome = function(palindrome) {
+    if(palindrome.length<=1) return ''
+    
+    for(let i=0;i<Math.floor(palindrome.length/2);i++){
+        if(palindrome[i]!=='a'){
+            return `${palindrome.slice(0,i)}a${palindrome.slice(i+1)}`
+        }
+    }
+    
+    return `${palindrome.slice(0,palindrome.length-1)}b`
+};
