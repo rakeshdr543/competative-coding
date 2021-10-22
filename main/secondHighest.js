f2f1740467059e508eabea2f4aa2d10c2bb1bda3
Rakesh D R <rakeshdr543@gmail.com>
2021-10-22 08:31:59 +0530

    Added second highest

---
+++ main/secondHighest.js
@@ -0,0 +1,7 @@
+// 4. Find the second highest element of the array using inbuild functions.
+
+const secondHighest=(arr)=>{
+    return arr.sort((a,b)=>b-a)[1]
+  }
+  
+  console.log(secondHighest([1,3,5,6,7]))
